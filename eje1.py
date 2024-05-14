7c0c7e82763c38a79baf642cd0d3fe32d95d7da5
rodrigochani <rodrigochani@live.com.ar>
2024-05-14 10:37:55 -0300

    Commit del primer ejercicio.

---
+++ eje1.py
@@ -0,0 +1,11 @@
+# ################################ Maestría en Aplicaciones de Información Espacial ################################
+# ################################ Introducción a las técnicas inteligentes de resolución de problemas de planificación, secuenciación y ejecución ################################
+# ################################ Trabajo práctico Nº: 5 ################################
+# ################################ Ejercicio 1: Escriba un programa en Python que imprima en consola el recorrido de una lista cuyos valores son: mascotas=["perro", "gato", "loro"]. ################################
+
+mascotas = ['Perro', 'Gato', 'Loro']
+
+for mascota in mascotas:
+    print("La mascota es un: ", mascota)
+
+# Modo de ejecución: abrir en terminal python con "./eje1.py", dentro de la carpeta contenedora del archivo.
